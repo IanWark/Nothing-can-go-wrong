d33ca4b0df1c194d235889bf25dec930bf07cfb9
IanWark <wark@ualberta.ca>
2025-02-21 23:53:29 -0500

    Make tank drive to a target point

---
+++ Nothing Can Go Wrong/Assets/MineGame/Scripts/Tank.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using Unity.FPS.Game;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Tank : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> m_pathPoints;
+
+    [SerializeField]
+    private NavMeshAgent m_navMeshAgent;
+
+    [SerializeField]
+    private Health m_health;
+
+    [SerializeField]
+    private GameObject m_aliveTankModel;
+
+    [SerializeField]
+    private GameObject m_destroyedTankModel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        m_health.OnDie += Die;
+
+        Go();
+    }
+
+    private void Go()
+    {
+        m_navMeshAgent.destination = m_pathPoints[0].position;
+    }
+
+    private void Die()
+    {
+        // TODO end the game
+        Destroy(m_aliveTankModel);
+        m_destroyedTankModel.SetActive(true);
+        m_navMeshAgent.isStopped = true;
+    }
+}
