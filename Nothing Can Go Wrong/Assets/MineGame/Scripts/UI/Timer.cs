ee76b033a70dcb8c7e3dcd415e37647b2172e0a0
IanWark <wark@ualberta.ca>
2025-02-22 19:19:49 -0500

    Add timer in top right

---
+++ Nothing Can Go Wrong/Assets/MineGame/Scripts/UI/Timer.cs
@@ -0,0 +1,37 @@
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public static string GetLastTimeElapsedFormatted()
+    {
+        return FormatTime(lastTimeElapsed);
+    }
+
+    private static float lastTimeElapsed = 0f;
+
+    [SerializeField]
+    private TextMeshProUGUI m_timerText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        lastTimeElapsed = 0f;
+        Timer.GetLastTimeElapsedFormatted();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        lastTimeElapsed += Time.deltaTime;
+
+        m_timerText.text = FormatTime(lastTimeElapsed);
+    }
+
+    private static string FormatTime(float time)
+    {
+        int minutes = (int)(time / 60);
+        int seconds = (int)(time % 60);
+        return string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
